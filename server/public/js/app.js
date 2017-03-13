ee603d548f0c90272fecba9262da2b4c550bafa0
Nate <sauceynate@gmail.com>
2017-03-13 14:12:21 -0500

    organized and commented hbs files

--- server/public/js/app.js
+++ server/public/js/app.js
@@ -1,8 +1,7 @@
-$('.form').find('input, textarea').on('keyup blur focus', function (e) {
-  
+//this makes it so the input text areas highlight so you can visually see where you are on the page
+$('.form').find('input, textarea').on('keyup blur focus', function (e) {  
   var $this = $(this),
       label = $this.prev('label');
-
 	  if (e.type === 'keyup') {
 			if ($this.val() === '') {
           label.removeClass('active highlight');
@@ -26,18 +25,12 @@ $('.form').find('input, textarea').on('keyup blur focus', function (e) {
     }
 
 });
-
-$('.tab a').on('click', function (e) {
-  
-  e.preventDefault();
-  
+//this flips between the log in and registration tabs on the home page
+$('.tab a').on('click', function (e) { 
+  e.preventDefault(); 
   $(this).parent().addClass('active');
-  $(this).parent().siblings().removeClass('active');
-  
+  $(this).parent().siblings().removeClass('active'); 
   target = $(this).attr('href');
-
-  $('.tab-content > div').not(target).hide();
-  
-  $(target).fadeIn(600);
-  
+  $('.tab-content > div').not(target).hide(); 
+  $(target).fadeIn(600);  
 });
