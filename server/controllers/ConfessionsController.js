337e3674206b828cf30994b592c47c5d527e32ed
Bridjet Mendyuk <bjmendy@Bridjets-MacBook-Pro.local>
2017-03-13 14:15:50 -0500

    controller update

---
+++ server/controllers/ConfessionsController.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var router = express.Router();
+var Confession = require('../models/Confession.js');
+
+router.get('/confessions', function(request, response){
+	console.log(request.session)
+	Confession.find(function(error, confessions){
+		console.log(confessions);
+		//searches the database
+	response.render('confessions', {confessionArray: confessions});
+	});
+}); 
+
+router.post('/', function(request, response){
+	var confession = new Confessions({name: request.body.username, confession: request.body.confession});
+	confession.save();
+	response.redirect("/confessions");
+
+});
+
+router.patch('/:id', function(request, response){
+var id = request.params.id;
+var newInfo = request.body;
+
+Confession.findById(id, function(err, confession){
+	confession.name = newInfo.username;
+
+		confession.save();
+		response.redirect('/confessions');
+})
+	});
+
+module.exports = router;
