0e706740a0073edd685df4f776e58ee65767d0a6
Terry Lippincott <zosoroxaxe@gmail.com>
2017-03-13 09:51:33 -0500

    basic downloads

---
+++ server/app.js
@@ -0,0 +1,10 @@
+var express		= require('express'),
+	app			= express(),
+	server		= require('http').createServer(app);
+var bodyParser	= require('body-parser');
+var path		= require('path');
+var mongoose	= require('mongoose');
+
+server.listen(3000, function() {
+	console.log('yup, server is listening on port 3000');
+})
