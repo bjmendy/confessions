5be3ba74361a0927a9dd579c6db4af7581f08a31
Nate <sauceynate@gmail.com>
2017-03-16 10:27:02 -0500

    Up to date latest patch working, need to work on getting posts onto the body of the page, logout button functionality

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -1,127 +1,119 @@
-var express	= require('express');
-var router 	= express.Router();
-var User 	= require('../models/User.js');
-var bcrypt 	= require('bcryptjs');
+var express    = require('express');
+var router     = express.Router();
+var User     = require('../models/User.js');
+var bcrypt     = require('bcryptjs');
 
 //app.use(/) this denotes that every route in this controller starts with /!!!
 
 router.get('/login', function(request, response) {
-	console.log(request.session);
-  	response.render('registerLogin', {});
+    console.log(request.session);
+     response.render('registerLogin', {});
 });
 
 router.get('/register', function(request, response) {
-	console.log(request.session);
-  	response.render('registerLogin', {notRegistered: true});
+    console.log(request.session);
+     response.render('registerLogin', {notRegistered: true});
 });
 
 
 router.post('/user/login', function(request, response){
-	// var user = new User({username: request.body.username, password: request.body.password});
-	// user.save();
+    // var user = new User({username: request.body.username, password: request.body.password});
+    // user.save();
 
-	// ////////////
+    // ////////////
 User.findOne({username: request.body.username}), function(error, user){
-	if (user){
+    if (user){
 
-		bcypt.compare(password, user.password, function(error, match){ //make an object
-			//this method returns true or false
-			//true the passwords match
-			if(match){
-				request.session.username = user.username
-				request.session.userId = user.id 
-				request.session.loggedIn = true
-				response.redirect('/confessions')
+        bcypt.compare(password, user.password, function(error, match){ //make an object
+            //this method returns true or false
+            //true the passwords match
+            if(match){
+                request.session.username = user.username
+                request.session.userId = user.id
+                request.session.loggedIn = true
+                response.redirect('/confessions')
 
-			}else{
-				response.render('/user/login', {message: 'username was taken!'})
-			}
-		})
-			}
+            }else{
+                response.render('/user/login', {message: 'username was taken!'})
+            }
+        })
+            }
 
-		};
+        };
 
 router.post('/login', function(request, response) {
-	console.log(request.body);
-	var password = request.body.password;
+    console.log(request.body);
+    var password = request.body.password;
 
-	User.findOne({username: request.body.username}, function(error, user) {
-		if (user) {
-			bcrypt.compare(password, user.password, function(error, match) {
-				// this method returns true or false
-				if (match) {
-					request.session.username = user.username;
-					request.session.userId = user.id;
-					request.session.loggedIn = true;
+    User.findOne({username: request.body.username}, function(error, user) {
+        if (user) {
+            bcrypt.compare(password, user.password, function(error, match) {
+                // this method returns true or false
+                if (match) {
+                    request.session.username = user.username;
+                    request.session.userId = user.id;
+                    request.session.loggedIn = true;
 
-					response.redirect('/confessions');  // successful login
-				}
-				else {
-					console.log('redirect login hit');
-					response.redirect('/user/login');
-				}
+                    response.redirect('/confessions');  // successful login
+                }
+                else {
+                    console.log('redirect login hit');
+                    response.redirect('/user/login');
+                }
 
-			})
-		}
-		else {
-			console.log('redirect register hit');
-			response.redirect('/user/register');
-		}
-	})
+            })
+        }
+        else {
+            console.log('redirect register hit');
+            response.redirect('/user/register');
+        }
+    })
 })
 
-<<<<<<< HEAD
-router.post('/user/register', function(request, response) {
-=======
 })
 
 router.post('/register', function(request, response) {
->>>>>>> 4d6bafd65191bad46b6939d8e7efd41005bcd845
-	console.log(request.body);
-	User.findOne({username: request.body.username}, function(error, user) {
-		if (!user) {
-			bcrypt.genSalt(10, function(error, salt) {
-				bcrypt.hash(request.body.password, salt, function(error, hash) {
-					var hashedPasswordObject = {};
+    console.log(request.body);
+    User.findOne({username: request.body.username}, function(error, user) {
+        if (!user) {
+            bcrypt.genSalt(10, function(error, salt) {
+                bcrypt.hash(request.body.password, salt, function(error, hash) {
+                    var hashedPasswordObject = {};
 
-					hashedPasswordObject.username = request.body.username;
-					hashedPasswordObject.password = hash;
+                    hashedPasswordObject.username = request.body.username;
+                    hashedPasswordObject.password = hash;
 
-					User.create(hashedPasswordObject, function(error, user) {
-						if (user) {
-							request.session.username	= user.username;
-							request.session.userId		= user.id;
-							request.session.loggedIn	= true;
+                    User.create(hashedPasswordObject, function(error, user) {
+                        if (user) {
+                            request.session.username    = user.username;
+                            request.session.userId        = user.id;
+                            request.session.loggedIn    = true;
 
-							response.redirect('/confessions');
-						}
-						else {
-							console.log(error);
-<<<<<<< HEAD
-=======
+                            response.redirect('/confessions');
+                        }
+                        else {
+                            console.log(error);
 
->>>>>>> 4d6bafd65191bad46b6939d8e7efd41005bcd845
-							response.redirect('/user/login');
-						}
+                            response.redirect('/user/login');
+                        }
 
-					})
-				})
+                    })
+                })
 
-			})
-		}
-	})
+            })
+        }
+    })
 })
-})
-// 		else {
-// 			response.render('registerLogin', {message: 'Sorry, that username is taken'});
-// 		}
-// 	})
+//         else {
+//             response.render('registerLogin', {message: 'Sorry, that username is taken'});
+//         }
+//     })
 // }
 
 router.get('/logout', function(request, response) {
-	request.session.destroy(function(error) {
-		response.redirect('/user/login');
-	})
+    request.session.destroy(function(error) {
+        response.redirect('/user/login');
+    })
 })
 
 module.exports = router;
